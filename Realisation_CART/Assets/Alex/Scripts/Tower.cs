46cce0d332a6e09636814aadef34b5fe34b720e7
rallardm@gmail.com <rallardm@gmail.com>
2024-03-22 15:51:01 -0400

    Adding physical material to the boxes physic Adding Remove/add spring when box are added or removed

--- Realisation_CART/Assets/Alex/Scripts/Tower.cs
+++ Realisation_CART/Assets/Alex/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using DiscountDelirium;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,20 +12,59 @@ public class Tower : MonoBehaviour
     void Start()
     {
         AddBoxToTower();
+        Box firstBox = m_boxesInCart.Peek();
+        GetComponent<DebugControls>().RB = firstBox.GetComponent<Rigidbody>();
     }
 
     public void AddBoxToTower()
     {
         m_boxCount++;
-        GameObject instant = Instantiate(boxPrefab);
+        Vector3 newBoxPos = transform.position;
+        Quaternion newBoxRot = Quaternion.identity;
+
+        if (m_boxCount > 1)
+        {
+            newBoxPos = new Vector3(
+                m_boxesInCart.ToArray()[0].transform.position.x,
+                m_boxesInCart.ToArray()[0].transform.position.y,
+                m_boxesInCart.ToArray()[0].transform.position.z);
+        }
+
+        GameObject instant = Instantiate(boxPrefab, newBoxPos, newBoxRot);
         instant.transform.SetParent(transform);
         instant.name = "Boxe " + m_boxCount;
         Box instantBox = instant.GetComponent<Box>();
         instantBox.SetTower(this);
+        float _x = transform.position.x;
+        float _z = transform.position.z;
         float height = (m_boxCount - 1) * boxHeight;
-        instant.transform.position = new Vector3(transform.position.x, height, transform.position.z);
+        Debug.Log("m_boxCount: " + m_boxCount);
+        if (m_boxCount > 1)
+        {
+            _x = m_boxesInCart.ToArray()[0].transform.position.x;
+            _z = m_boxesInCart.ToArray()[0].transform.position.z;
+            Box newBoxe = m_boxesInCart.ToArray()[m_boxesInCart.Count - 1];
+            SpringJoint springJoint = instant.AddComponent<SpringJoint>();
+            springJoint.connectedBody = newBoxe.GetComponent<Rigidbody>();
+            springJoint.spring = 5;
+            springJoint.damper = 0;
+            springJoint.minDistance = 0;
+            springJoint.maxDistance = 0.2f;
+            springJoint.tolerance = 0.06f;
+            springJoint.enableCollision = true;
+        }
+        
+        if (m_boxCount > 2)
+        {
+            Debug.Log("m_boxCount > 2: " + m_boxesInCart.Count);
+            Box previousBox = m_boxesInCart.ToArray()[0];
+            Debug.Log("previousBox: " + previousBox.name);
+            SpringJoint previousSpringJoint = previousBox.GetComponent<SpringJoint>();
+            previousSpringJoint.spring = 100;
+        }
+
+        instant.transform.position = new Vector3(_x, height, _z);
         m_boxesInCart.Push(instantBox);
-       // Debug.Log("boxes in tower: " + m_boxesInCart.Count);
     }
 
     public void RemoveBoxToTower()
@@ -38,12 +78,22 @@ public class Tower : MonoBehaviour
         m_boxCount--;
         Box boxToRemove = m_boxesInCart.Pop();
         Destroy(boxToRemove.gameObject);
-       // Debug.Log("boxes in tower: " + m_boxesInCart.Count);
+
+        if (m_boxCount > 2)
+        {
+            Debug.Log("m_boxCount > 2: " + (m_boxesInCart.Count - 1));
+            Box currentTopBox = m_boxesInCart.ToArray()[0];
+            Debug.Log("previousBox: " + currentTopBox.name);
+            SpringJoint previousSpringJoint = currentTopBox.GetComponent<SpringJoint>();
+            previousSpringJoint.spring = 5;
+        }
+
+        // Debug.Log("boxes in tower: " + m_boxesInCart.Count);
     }
 
     // TEST
     private void Update()
-    {     
+    {
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
             AddBoxToTower();
