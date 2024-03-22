c25faac39a442c018af3d7a05909ce2b858faad2
rallardm@gmail.com <rallardm@gmail.com>
2024-03-22 12:04:02 -0400

    Adding Debug Controls

---
+++ Realisation_CART/Assets/Remi/Scripts/DebugControls.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DiscountDelirium
+{
+    public class DebugControls : MonoBehaviour
+    {
+        [field: SerializeField]private Rigidbody RB { get; set; }
+        [field: SerializeField]private Camera Camera { get; set; }
+        private float m_speed = 500.0f;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 newDirection = Vector3.zero;
+            Vector3 cameraForward = Vector3.ProjectOnPlane(Camera.transform.forward, Vector3.up).normalized;
+            Vector3 cameraRight = Vector3.ProjectOnPlane(Camera.transform.right, Vector3.up).normalized;
+
+            if (!Input.anyKey)
+            {
+                RB.velocity = Vector3.zero;
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                //Debug.Log("Pressed W");
+                newDirection += cameraForward * m_speed;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                //Debug.Log("Pressed S");
+                newDirection -= cameraForward * m_speed;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                //Debug.Log("Pressed A");
+                newDirection -= cameraRight * m_speed;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                //Debug.Log("Pressed D");
+                newDirection += cameraRight * m_speed;
+            }
+
+            RB.AddForce(newDirection, ForceMode.Acceleration);
+        }
+    }
+}
