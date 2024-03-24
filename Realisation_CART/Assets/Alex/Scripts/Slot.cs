62b8fd9c92ad228fc6e1b2f6941a95516cd09917
rallardm@gmail.com <rallardm@gmail.com>
2024-03-24 02:58:35 -0400

    Revert slot debug

--- Realisation_CART/Assets/Alex/Scripts/Slot.cs
+++ Realisation_CART/Assets/Alex/Scripts/Slot.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
@@ -15,29 +15,14 @@ public class Slot : MonoBehaviour
     private void Update()
     {
         // Sert a tester le placement de slot avec rendu au gameplay (Scene)
-        //Debug.Log("transform.parent.parent.position " + transform.parent.parent.position);
         Vector3 slotPosition = transform.parent.parent.position + transform.localPosition;
-        //transform.rotation = transform.parent.parent.rotation;
-
-        Debug.DrawLine(transform.position, transform.position + Vector3.up, Color.black);
+        Debug.DrawLine(slotPosition, slotPosition + Vector3.up, Color.black);
     }
 
-    //private void Update()
-    //{
-    //    // Sert a tester le placement de slot avec rendu au gameplay (Scene)
-    //    //Debug.Log("transform.parent.parent.position  " + transform.parent.parent.position);
-    //    //Debug.Log("transform.parent.parent.name  " + transform.parent.parent.name);
-    //    //Debug.Log("transform.name  " + transform.name);
-    //    Vector3 slotPosition = transform.parent.parent.parent.position + transform.localPosition;
-    //    //Debug.Log("slotPosition  " + slotPosition);
-    //    //transform.rotation = transform.parent.parent.rotation;
-    //    Debug.DrawLine(slotPosition, slotPosition + Vector3.up, Color.black);
-    //}
-
-    /// <summary> Donne une couleur random a la slot spécial </summary>
+    /// <summary> Donne une couleur random a la slot spï¿½cial </summary>
     Color GetRandomColor(int value)
     {
-        return new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1) ;
+        return new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1);
     }
 
 }
