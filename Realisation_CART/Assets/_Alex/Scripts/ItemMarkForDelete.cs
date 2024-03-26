d108a1ebe86d57046b64e9a247c0a44a3b316e38
legendalex999 <legendalex999@gmail.com>
2024-03-26 15:35:41 -0400

    ItemMarkForDelete

---
+++ Realisation_CART/Assets/_Alex/Scripts/ItemMarkForDelete.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace DiscountDelirium
+{
+    public class ItemMarkForDelete : MonoBehaviour
+    {
+        [SerializeField] private float m_delayUntilDelete;
+
+        private void OnEnable()
+        {
+            Invoke("DestroyItem", m_delayUntilDelete);
+        }
+
+        private void DestroyItem()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
