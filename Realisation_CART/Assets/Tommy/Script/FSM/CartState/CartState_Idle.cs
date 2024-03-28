2de82eecc1c82ec0a9229bb696f75f523351cd74
rallardm@gmail.com <rallardm@gmail.com>
2024-03-27 22:54:35 -0400

    revert uneeded changes

--- Realisation_CART/Assets/Tommy/Script/FSM/CartState/CartState_Idle.cs
+++ Realisation_CART/Assets/Tommy/Script/FSM/CartState/CartState_Idle.cs
@@ -5,10 +5,18 @@ namespace CartControl
 	public class CartState_Idle : CartState
 	{
 		//m_cartStateMachine;
+		private bool m_firstTime = true;
 
 		public override void OnEnter()
 		{
 			Debug.LogWarning("current state: IDLE");
+			
+			if(m_firstTime)
+			{
+				m_firstTime = false;
+				return;
+			}
+
 		}
 
 		public override void OnUpdate()
@@ -29,19 +37,21 @@ namespace CartControl
 
 		public override bool CanEnter(IState currentState)
 		{
+			if(currentState is CartState_TurnInPlace)
+			{
+				return (m_cartStateMachine.SteeringValue < 0 + GameConstants.DEADZONE && m_cartStateMachine.SteeringValue > 0 - GameConstants.DEADZONE);
+				
+			}
+
 			if(m_cartStateMachine.CanBoost)
 			{
 				return false;
 			}
 
-			if (m_cartStateMachine.ForwardPressedPercent < GameConstants.DEADZONE
+			return (m_cartStateMachine.ForwardPressedPercent < GameConstants.DEADZONE
 				&& m_cartStateMachine.BackwardPressedPercent < GameConstants.DEADZONE
-				&& m_cartStateMachine.m_cartRB.velocity.magnitude < GameConstants.DEADZONE)
-			{
-				return true;
-			}
-
-			return false;
+				&& m_cartStateMachine.CartRB.velocity.magnitude < GameConstants.DEADZONE);
+			
 		}
 
 		public override bool CanExit()
