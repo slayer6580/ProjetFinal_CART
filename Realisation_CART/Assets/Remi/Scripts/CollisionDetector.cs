071249b4b075eab816da04abdc9c33683328761a
rallardm@gmail.com <rallardm@gmail.com>
2024-03-28 22:22:47 -0400

    Main_Remi update

--- Realisation_CART/Assets/Remi/Scripts/CollisionDetector.cs
+++ Realisation_CART/Assets/Remi/Scripts/CollisionDetector.cs
@@ -9,31 +9,34 @@ namespace BoxSystem
 
         private TowerPhysics _TowerPhysics { get; set; } = null;
         private const float MAX_VEL_TO_DROP_CONTENT = 15f;
+        private int m_playerLayer = 6;
         private int m_clientLayer = 7;
+        private int m_boxLayer = 9;
         private int m_groundLayer = 10;
 
         private void Start()
         {
-            _TowerPhysics = transform.parent.GetComponentInChildren<TowerPhysics>();
+            _TowerPhysics = GetComponentInChildren<TowerPhysics>();
             if (_TowerPhysics == null) Debug.LogError("TowerPhysics not found in parent");
         }
 
         private void OnCollisionEnter(Collision collision)
         {
-            if (collision.gameObject.layer == m_clientLayer)
+            if (collision.gameObject.layer == m_clientLayer && gameObject.layer == m_playerLayer)
             {
                 if (collision.relativeVelocity.magnitude < MAX_VEL_TO_DROP_CONTENT) return;
 
                 Vector3 velocity = collision.relativeVelocity;
                 _TowerPhysics.CheckIfCanDropContent(velocity);
             }
-            else if (collision.gameObject.layer == m_groundLayer)
+            else if (collision.gameObject.layer == m_groundLayer && gameObject.layer == m_boxLayer)
             {
-                if (GetComponent<AutoDestruction>().enabled) return;
+                AutoDestruction autoDestruction = GetComponent<AutoDestruction>();
+                if (autoDestruction != null && autoDestruction.enabled) return;
 
                 Debug.Log("Collision with ground: " + gameObject.name);
                 GetComponent<Box>().GetTower().RemoveLastBoxFromTower();
-                GetComponent<AutoDestruction>().enabled = true;
+                autoDestruction.enabled = true;
             }
         }
     }
