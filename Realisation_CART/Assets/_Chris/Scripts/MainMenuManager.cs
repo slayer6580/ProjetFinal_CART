5b0aa55efac88c25b22cb449d7b59aad84830ea2
Christopher M <44980780+slayer6580@users.noreply.github.com>
2024-04-02 16:33:41 -0400

    +Added Base MainMenu scene + UI + Navigation +Added OptionsMenu with volume sliders

---
+++ Realisation_CART/Assets/_Chris/Scripts/MainMenuManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DiscountDelirium
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [Header("Menus")]
+        [SerializeField] private GameObject m_mainMenu;
+        [SerializeField] private GameObject m_optionsMenu;
+
+        [Header("Volume Sliders")]
+        [SerializeField] private Slider m_musicSlider;
+        [SerializeField] private Slider m_soundSlider;
+
+        [SerializeField] private float m_musicVolume;
+        [SerializeField] private float m_soundVolume;
+
+        public void StartGame()
+        {
+            Debug.Log("StartGame");
+        }
+
+        public void OptionsMenu(bool active) 
+        {
+            m_mainMenu.SetActive(!active);
+            m_optionsMenu.SetActive(active);
+        }
+
+        public void SetVolume() 
+        {
+            m_musicVolume = m_musicSlider.value;
+            m_soundVolume = m_soundSlider.value;
+        }
+
+        public void QuitGame() 
+        {
+            Debug.Log("QuitGame");
+            Application.Quit();
+        }
+    }
+}
