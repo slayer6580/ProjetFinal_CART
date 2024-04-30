e7ff41954aac57c26ea9abc991b903d7a7814ac6
MACHINE_A_ALEX\legen <legendalex999@gmail.com>
2024-04-30 13:36:02 -0400

    Push little fix

--- Realisation_CART/Assets/_Alex/Scripts/SaveAndLoad.cs
+++ Realisation_CART/Assets/_Alex/Scripts/SaveAndLoad.cs
@@ -5,13 +5,14 @@ namespace SavingSystem
 {
     public class SaveAndLoad : MonoBehaviour
     {
-        private const string SAVE_LOCATION = "/_Alex/save.txt";
+        private const string SAVE_LOCATION_TEST = "/_Alex/save.txt";
+        private const string LEADERBOARD_LOCATION = "/_Alex/LeaderBoardSave.txt";
 
         private void Awake()
         {
             SaveTest saveTest = new SaveTest { test = 4 };
-
             SaveTest(saveTest);
+
             SaveTest newLoad = LoadTest();
             Debug.Log("Loaded value: " + newLoad.test);
         }
@@ -19,16 +20,14 @@ namespace SavingSystem
         public void SaveTest(SaveTest objectToSave)
         {
             string jsonString = JsonUtility.ToJson(objectToSave);
-
-            File.WriteAllText(Application.dataPath + SAVE_LOCATION, jsonString);
-           
+            File.WriteAllText(Application.dataPath + SAVE_LOCATION_TEST, jsonString);           
         }
 
         public SaveTest LoadTest()
         {
-            if (File.Exists(Application.dataPath + SAVE_LOCATION))
+            if (File.Exists(Application.dataPath + SAVE_LOCATION_TEST))
             {
-                string jsonString = File.ReadAllText(Application.dataPath + SAVE_LOCATION);
+                string jsonString = File.ReadAllText(Application.dataPath + SAVE_LOCATION_TEST);
                 return JsonUtility.FromJson<SaveTest>(jsonString);
             }
             else
@@ -38,6 +37,25 @@ namespace SavingSystem
             }
         }
 
+        public void SaveLeaderboard(SavedLeaderboard leaderboard)
+        {
+            string jsonString = JsonUtility.ToJson(leaderboard);
+            File.WriteAllText(Application.dataPath + LEADERBOARD_LOCATION, jsonString);
+        }
+
+        public SavedLeaderboard LoadLeaderboard()
+        {
+            if (File.Exists(Application.dataPath + LEADERBOARD_LOCATION))
+            {
+                string jsonString = File.ReadAllText(Application.dataPath + SAVE_LOCATION_TEST);
+                return JsonUtility.FromJson<SavedLeaderboard>(jsonString);
+            }
+            else
+            {
+                Debug.Log("No save");
+                return null;
+            }
+        }
 
     }
 
