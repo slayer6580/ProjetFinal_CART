f3a5904f8b7d47502ec48041a067e4168179dc7b
Remi A. M. <rallardm@gmail.com>
2024-05-29 10:26:07 -0400

    Commenting out debug logs

--- Realisation_CART/Assets/Tommy/Script/BehaviourTree/Nodes/CheckIfStuck.cs
+++ Realisation_CART/Assets/Tommy/Script/BehaviourTree/Nodes/CheckIfStuck.cs
@@ -17,44 +17,44 @@ namespace BehaviourTree
 
 		protected override State OnUpdate()
 		{
-			Debug.Log("AAA");
+			//Debug.Log("AAA");
 			if (m_blackboard.m_cartStateMachine.LocalVelocity.z < m_underSpeedToCountAsStuck && m_blackboard.m_cartStateMachine.LocalVelocity.z > -m_underSpeedToCountAsStuck && Time.time > 5)
 			{
-				Debug.Log("BBB");
+				//Debug.Log("BBB");
 				if (m_blackboard.m_stuckAtTime == 0)
 				{
-					Debug.Log("CCC");
+					//Debug.Log("CCC");
 					m_blackboard.m_stuckAtTime = Time.time;
 					m_blackboard.m_timeStuck = 0;
 				}
 				else
 				{
-					Debug.Log("DDD");
+					//Debug.Log("DDD");
 					m_blackboard.m_timeStuck = Time.time - m_blackboard.m_stuckAtTime;
 				}
 
 			}
 			else
 			{
-				Debug.Log("EEE");
+				//Debug.Log("EEE");
 				m_blackboard.m_stuckAtTime = 0;
 				m_blackboard.m_timeStuck = 0;
 			}
 
 			if(m_blackboard.m_timeStuck > m_timeBeforeUnstuck)
 			{
-				Debug.Log("FFF");
+				//Debug.Log("FFF");
 				return m_children[0].Update();				
 			}
 			else
 			{
 				if(m_children.Count > 1)
 				{
-					Debug.Log("GGG");
+					//Debug.Log("GGG");
 					return m_children[1].Update();
 				}
 			}
-			Debug.Log("HHH");
+			//Debug.Log("HHH");
 			return State.Success;
 		}
 	}
