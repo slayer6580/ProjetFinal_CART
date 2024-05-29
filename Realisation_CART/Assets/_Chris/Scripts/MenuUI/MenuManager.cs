b8386d648c1135dd0c45176c7c2d94fd9a0f74db
Remi A. M. <rallardm@gmail.com>
2024-05-29 11:43:16 -0400

    Last correction level 2 baking

--- Realisation_CART/Assets/_Chris/Scripts/MenuUI/MenuManager.cs
+++ Realisation_CART/Assets/_Chris/Scripts/MenuUI/MenuManager.cs
@@ -114,7 +114,7 @@ namespace DiscountDelirium
 
         public void SetSoundVolume()
         {
-            Debug.Log("Sound Volume: " + m_soundSlider.value);
+            //Debug.Log("Sound Volume: " + m_soundSlider.value);
             PlayerPrefs.SetFloat("SoundVolume", m_soundSlider.value);
             for (int i = 0; i < _AudioManager.GetAudioBox().Count; i++)
             {
